987ab633038a6a4a6b8b8d4da8d757479c4b0891
kusmangalievasanali <kusmangalievasanali@gmail.com>
2026-09-13 01:12:42 +0500

    Update RobotPatternsApplication.java

--- src/main/java/com/robotbuilder/robot_patterns/RobotPatternsApplication.java
+++ src/main/java/com/robotbuilder/robot_patterns/RobotPatternsApplication.java
@@ -7,7 +7,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RobotPatternsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RobotPatternsApplication.class, args);
+        Robot robotCleaner = new RobotBuilder()
+                .setModelName("Tazadom")
+                .setChassisType("track")
+                .setBatteryModel("Barys")
+                .setBatteryCapacity(100)
+                .setHasManipulator(true)
+                .setPrimarySensor("Ultrasonic")
+                .build();
+
+
+        System.out.println("___RobotCleaner___");
+        System.out.println(robotCleaner);
+
+
+        //With director
+        RobotDirector director = new RobotDirector();
+
+        Robot explorer = director.constructExplorerRobot(new RobotBuilder());
+        System.out.println("___Explorer Robot by director___");
+        System.out.println(explorer);
+
+
+        Robot delivery = director.constructDeliveryRobot(new RobotBuilder());
+        System.out.println("___Delivery Robot by director___");
+        System.out.println(delivery);
+
+
+
+
+
     }
+
+
 
 }
